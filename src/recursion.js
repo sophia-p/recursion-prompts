c377c4a1933d2695321e8a59718362637fa7c0ef
Sophia Pignatelli <pignatelli@fordham.edu>
2017-08-18 22:10:29 -0400

    sumBelow recursive, a bit hacky

--- src/recursion.js
+++ src/recursion.js
@@ -56,10 +56,16 @@ var sumBelow = function(n) {
 	if (n ===0) {
 		return 0;
 	} else {
-		return n + sumBelow(n-1);
-	}
-};
 
+		var ans = Math.abs(n)-1 + sumBelow(Math.abs(n)-1);
+		if (n < 0) {
+		  return ans * -1;
+		} else {
+		  return ans;
+		}
+	}
+
+};
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
