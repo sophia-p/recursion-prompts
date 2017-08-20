50a43953971205b2a8ece95634d77380b84312a9
Sophia Pignatelli <pignatelli@fordham.edu>
2017-08-19 23:58:59 -0400

    palindrome recursive

--- src/recursion.js
+++ src/recursion.js
@@ -134,6 +134,16 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+	string = string.toLowerCase().trim();
+	if (string.length <= 1){
+		return true;
+	} else {
+		if (string.charAt(0) !== string.charAt(string.length-1)) {
+			return false;
+		} else {
+			return palindrome(string.substring(1,string.length-1));
+		}
+	}
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
