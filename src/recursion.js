6665d42bae2974a5a090490c1a9faa6b85dc7263
Sophia Pignatelli <pignatelli@fordham.edu>
2017-08-19 23:33:55 -0400

    powerOfTwo recursive

--- src/recursion.js
+++ src/recursion.js
@@ -114,6 +114,13 @@ var exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
+	if (n ===2 || n === 1){
+		return true;
+	} else if (n > 2){
+		return powerOfTwo(n/2);
+	} else {
+		return false;
+	}
 };
 
 // 9. Write a function that reverses a string.
