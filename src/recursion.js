bd85891bac33a198ab70a0b65ddfd76afc3e2862
Sophia Pignatelli <pignatelli@fordham.edu>
2017-08-20 21:53:10 -0400

    gcd recursive

--- src/recursion.js
+++ src/recursion.js
@@ -186,6 +186,13 @@ var divide = function(x, y) {
 // http://www.cse.wustl.edu/~kjg/cse131/Notes/Recursion/recursion.html
 // https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
 var gcd = function(x, y) {
+	if (x < 0 || y < 0) {
+		return null;
+	} else if ((x % y) === 0) {
+		return y;
+	} else {
+		return gcd(y, x%y);
+	}
 };
 
 // 15. Write a function that compares each character of two strings and returns true if
